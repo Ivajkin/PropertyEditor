5589522f256137fafc14d9fee9437c0ca8ee39d7
Ivajkin Timofej <timatnet@gmail.com>
2012-08-25 10:55:51 +1100

    Few tests added

--- Project/PropertyEditor/PropertyEditor.Tests/Controllers/PropertyEditorControllerTest.cs
+++ Project/PropertyEditor/PropertyEditor.Tests/Controllers/PropertyEditorControllerTest.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PropertyEditor;
 using PropertyEditor.Controllers;
+using PropertyEditor.Models;
 
 namespace PropertyEditor.Tests.Controllers
 {
@@ -15,14 +16,14 @@ namespace PropertyEditor.Tests.Controllers
         [TestMethod]
         public void Index()
         {
-            /*// Arrange
+            // Arrange
             PropertyEditorController controller = new PropertyEditorController();
 
             // Act
-            ViewResult result = controller.Index((int)PropertyEditorController.Status.ready) as ViewResult;
+            ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-           // Assert.AreEqual("Welcome to ASP.NET MVC!", result.ViewBag.Message);*/
+            Assert.AreEqual("Welcome to ASP.NET MVC!", result.ViewBag.Message);
         }
 
         [TestMethod]
@@ -45,7 +46,7 @@ namespace PropertyEditor.Tests.Controllers
             PropertyEditorController controller = new PropertyEditorController();
 
             // Act
-            ViewResult result = controller.Add(new PropertyEditor.Models.Property()) as ViewResult;
+            ViewResult result = controller.Add(new Property { Name = "TEST_PROPERTY", Type = (int)property_type.integer_type, Value = "128" }) as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
@@ -58,25 +59,35 @@ namespace PropertyEditor.Tests.Controllers
             PropertyEditorController controller = new PropertyEditorController();
 
             // Act
-            //ViewResult result = controller.Add(new PropertyEditor.Models.Property()) as ViewResult;
-            // Act
-            //result = controller.Delete("width") as ViewResult;
+            ViewResult result = controller.Add(new Property { Name = "TEST_PROPERTY2", Type = (int)property_type.string_type, Value = "test" }) as ViewResult;
 
             // Assert
-            //Assert.IsNotNull(result);
+            Assert.IsNotNull(result);
+
+            // Act
+            result = controller.Delete("TEST_PROPERTY2") as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
         }
 
         [TestMethod]
         public void EditPanel()
         {
-            /*// Arrange
+            // Arrange
             PropertyEditorController controller = new PropertyEditorController();
 
             // Act
-            ViewResult result = controller.EditPanel("height") as ViewResult;
+            ViewResult result = controller.Add(new Property { Name = "TEST_PROPERTY3", Type = (int)property_type.integer_type, Value = "256" }) as ViewResult;
 
             // Assert
-            Assert.IsNotNull(result);*/
+            Assert.IsNotNull(result);
+            
+            // Act
+            result = controller.EditPanel("TEST_PROPERTY3") as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
         }
 
         [TestMethod]
@@ -84,9 +95,16 @@ namespace PropertyEditor.Tests.Controllers
         {
             // Arrange
             PropertyEditorController controller = new PropertyEditorController();
+            Property property = new Property { Name = "TEST_PROPERTY4", Type = (int)property_type.integer_type, Value = "500" };
 
             // Act
-            ViewResult result = controller.Edit(new PropertyEditor.Models.Property()) as ViewResult;
+            ViewResult result = controller.Add(property) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+
+            // Act
+            result = controller.Edit(property) as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
